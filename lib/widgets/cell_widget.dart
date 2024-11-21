03464e73c9b5bbd4d4af4f472534c91fb99c8466
@nuez0 <veroagug@gmail.com>
2024-11-21 17:03:20 -0600

    Mejoras a nivel de celdas

--- lib/widgets/cell_widget.dart
+++ lib/widgets/cell_widget.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import '../models/cell.dart';
 import '../models/symbol.dart';
 
-class CellWidget extends StatelessWidget {
+class CellWidget extends StatefulWidget {
   final Cell cell;
   final VoidCallback onTap;
 
   CellWidget({required this.cell, required this.onTap});
 
   @override
+  _CellWidgetState createState() => _CellWidgetState();
+}
+
+class _CellWidgetState extends State<CellWidget> {
+  bool isTapped = false;
+
+  @override
   Widget build(BuildContext context) {
     String displaySymbol;
-    // Map the Symbol enum to displayable characters or icons
-    switch (cell.symbol) {
+    // Mapear el símbolo al carácter o icono correspondiente
+    switch (widget.cell.symbol) {
       case Symbol.rock:
         displaySymbol = '🪨';
         break;
@@ -38,10 +45,43 @@ class CellWidget extends StatelessWidget {
         displaySymbol = '';
     }
 
+    // Determinar el color de la celda
+    Color cellColor;
+
+    if (widget.cell.symbol != null) {
+      // Si la celda tiene un símbolo, usar un color definido (por ejemplo, azul claro)
+      cellColor = Colors.grey.withOpacity(0.5);
+    } else if (isTapped) {
+      // Si la celda está siendo tocada, usar un color con opacidad
+      cellColor = Colors.grey.withOpacity(0.5);
+    } else {
+      // Color por defecto
+      cellColor = Colors.white;
+    }
+
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
+      onTapDown: (_) {
+        setState(() {
+          isTapped = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          isTapped = false;
+        });
+        if (widget.cell.symbol == null) {
+          widget.onTap();
+        }
+      },
+      onTapCancel: () {
+        setState(() {
+          isTapped = false;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
         decoration: BoxDecoration(
+          color: cellColor,
           border: Border.all(color: Colors.black),
         ),
         child: Center(
